af261dda4704307358c01c17a736ab152f43c1c9
tavo-uemg <153575281+tavo-uemg@users.noreply.github.com>
2025-05-12 22:13:31 +0000

    fix: corrigir a definição do método prepend_http para ser um método de classe

--- src/brave/types/shared/custom_url.py
+++ src/brave/types/shared/custom_url.py
@@ -4,6 +4,7 @@ class AutoHttpUrl(BaseModel):
     url: HttpUrl
 
     @field_validator('url', mode='before')
+    @classmethod
     def prepend_http(cls, v):
         if isinstance(v, str) and not v.startswith('http'):
             return f'https://{v}'
